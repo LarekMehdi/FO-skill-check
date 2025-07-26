a06ef01041ffacb06b9f2524657cbd2cf44ef5ba
mehdi-larek <larek.med@gmail.com>
2025-07-26 10:41:07 +0200

    navbar + bootstrap

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+export default {
+    mounted() {
+        this.initNavbar();
+    },
+    data() {
+        return {}
+    },
+    components: {},
+    computed: {
+
+    },
+    methods: {
+        initNavbar() {
+            console.log('init');
+        }
+    }
+}
+
+</script>
+
+<template>
+    <nav class="navbar fixed-top navbar-expand navbar-dark bg-dark px-3">
+        <router-link to="/" class="navbar-brand">Accueil</router-link> |
+        <router-link to="/signin" class="navbar-brand">Se connecter</router-link> |
+        <router-link to="/signup" class="navbar-brand">Créer un compte</router-link> |
+  </nav>
+</template>
---
+++ src/components/Signup.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+export default {
+    mounted() { },
+    data() {
+        return {
+            
+        }
+    },
+    components: {},
+    computed: {
+
+    },
+    methods: {}
+}
+
+</script>
+
+<template>
+    <main>
+        <h1>Créer un compte</h1>
+    </main>
+</template>
