29b7fc20b7c57fb0c6f46fff3938e8fbcbdd49d9
mehdi-larek <larek.med@gmail.com>
2025-07-29 09:44:34 +0200

    logo + WIP home

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,6 +1,18 @@
 <script lang="ts">
+import { useAuth } from '../../composables/useAuth';
+import logo from '../../assets/logo-skill-check.png'
+
 
 export default {
+    setup() {
+        const { authStore, isLoggedIn, isAdmin } = useAuth();
+        return { 
+            authStore,
+            isLoggedIn,
+            isAdmin,
+            logo,
+        };
+    },
     mounted() {
         
         this.initHome();
@@ -24,7 +36,44 @@ export default {
 </script>
 
 <template>
-    <main>
-        <h1>Home</h1>
+    <main class="mt-5">
+        
+        <img :src="logo" alt="Logo SkillCheck" class="img-fluid my-3 d-block mx-auto" style="max-width: 200px;"/>
+        
+        <div v-if="!isLoggedIn">
+            <h3>Votre plateforme d'entraînement aux tests techniques</h3>
+            <p>
+                Vous préparez un entretien ou souhaitez évaluer vos compétences techniques? SkillCheck est conçu pour vous aider à progresser efficacement.
+            </p>
+            <p>
+                <strong>Des tests ciblés: </strong><br/>
+                Entraînez-vous avec des questions minutées regroupées par thématique ou niveau de difficulté. Chaque test est conçu pour simuler les conditions réelles des épreuves techniques.
+            </p>
+            <p>
+                <strong>Ajout de contenu par les admins: </strong><br/>
+                De nouvelles questions et tests sont régulièrement ajoutés pour couvrir un large éventail de sujets.
+            </p>
+            <p>
+                <strong>Suivi personnalisé: </strong><br/>
+                Consultez vos résultats détaillés, suivez votre progression et identifiez vos axes d’amélioration.
+
+            </p>
+            <p>
+                <strong>Statistiques globales: </strong><br/>
+                Comparez vos performances à celles des autres utilisateurs sur chaque test.
+            </p>
+            
+            <h6 class="text-primary">
+                <strong>Prêt à tester vos compétences ? Créez un compte, lancez un test et améliorez-vous dès aujourd’hui!</strong>
+            </h6>
+        </div>
+
+        <div v-if="isAdmin">
+
+        </div>
+
+        <div v-else>
+
+        </div>
     </main>
 </template>
