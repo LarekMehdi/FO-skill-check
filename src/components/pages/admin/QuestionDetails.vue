32f07622b6291aed32decdca6a463af3ba2ddd58
mehdi-larek <larek.med@gmail.com>
2025-09-03 15:38:27 +0200

    ajout d'infos sur le détails

--- src/components/pages/admin/QuestionDetails.vue
+++ src/components/pages/admin/QuestionDetails.vue
@@ -1,17 +1,22 @@
 <script lang="ts">
 import { useToast } from 'vue-toastification';
-import { Difficulty } from '../../../constants/difficulty.constant';
+import { Difficulty, getDifficultyOptions } from '../../../constants/difficulty.constant';
 import type { QuestionDetailsInterface } from '../../../interfaces/question.interface';
 import { QuestionService } from '../../../services/QuestionService';
 import InputTextArea from '../../ui/InputTextArea.vue';
 import InputText from '../../ui/InputText.vue';
 import CodeBlock from '../../ui/CodeBlock.vue';
+import InputSwitch from '../../ui/InputSwitch.vue';
+import InputNumber from '../../ui/InputNumber.vue';
+import InputSelect from '../../ui/InputSelect.vue';
 
     export default {
         setup() {
             const toast = useToast();
+            const difficultyOptions = getDifficultyOptions();
             return {
                 toast,
+                difficultyOptions,
             }
         },
         data(): { questionId: number, item: QuestionDetailsInterface}
@@ -52,6 +57,9 @@ import CodeBlock from '../../ui/CodeBlock.vue';
             InputTextArea,
             InputText,
             CodeBlock,
+            InputSwitch,
+            InputNumber,
+            InputSelect,
         }
     }
 </script>
@@ -62,6 +70,75 @@ import CodeBlock from '../../ui/CodeBlock.vue';
     <article>
 
         <section class="row mb-3">
+            <div class="col-md-6 text-start">
+                <InputSwitch
+                    v-model="item.isMultipleAnswer"
+                    name="isCorrect"
+                    label="Plusieurs bonne réponses?"
+                    :disabled="true"
+                />
+            </div>
+            <div class="col-md-6">
+                <InputNumber
+                    v-model="item.timeLimit"
+                    name="timeLimit"
+                    label="Limite de temps"
+                    :symbol="'secondes'"
+                    :disabled="true"
+                />
+            </div>
+        </section>
+
+        <section class="row mb-3">
+            <div class="col-md-4">
+                <InputNumber
+                    v-model="item.doneCount"
+                    name="doneCount"
+                    label="Tentatives"
+                    :disabled="true"
+                />
+            </div>
+            <div class="col-md-4">
+                <InputNumber
+                    v-model="item.correctCount"
+                    name="correctCount"
+                    label="Réussites"
+                    :disabled="true"
+                />
+            </div>
+            <div class="col-md-4">
+                <InputNumber
+                    v-model="item.successRate"
+                    name="successRate"
+                    label="Taux de réussite"
+                    :symbol="'%'"
+                    :disabled="true"
+                />
+            </div>
+        </section>
+
+        <section class="row mb-3">
+            <div class="col-md-6">
+                <InputSelect
+                    v-model="item.difficulty"
+                    name="difficulty"
+                    label="Difficulté"
+                    :disabled="true"
+                    :options="difficultyOptions"
+                />
+            </div>
+            <div class="col-md-6">
+                <InputText
+                    v-model="item.createdBy.pseudo"
+                    name="createdByPseudo"
+                    label="Créée par"
+                    :disabled="true"
+                />
+            </div>
+        </section>
+        
+
+        <section class="row mb-3">
             <div class="col-md-12">
                 <InputTextArea
                         v-model="item.content"
@@ -70,6 +147,7 @@ import CodeBlock from '../../ui/CodeBlock.vue';
                         :disabled="true"
                         :displayLabel="false"
                         :cols="70"
+                        :rows="3"
                     />
             </div>
         </section>
